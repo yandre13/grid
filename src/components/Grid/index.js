55c922a9e88ad226069da7bde5a88ef184cf93c1
yaser <yaser@pasaportestudio.com>
2021-05-08 16:43:02 -0500

    fix 5

--- src/components/Grid/index.js
+++ src/components/Grid/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Grid() {
 	return (
-		<div className="wrapper-grid">
+		<div className="wrapper-grid pointer-events-none hidden md:md-block">
 			<div className="h-full w-full absolute flex flex-wrap">
 				<div className="w-0 h-full inline-block md:md-box-y lg:lg-box-y xl:xl-box-y bgc-y"></div>
 				<div className="w-0 h-full inline-block md:md-box-y lg:lg-box-y xl:xl-box-y bgc-y"></div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,15 +3,6 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
-    container: {
-        center: true,
-        padding: {
-            DEFAULT: '1rem',
-            lg: '2rem',
-            xl: '6rem',
-            '2xl': '12rem',
-        },
-    },
   },
   variants: {
     extend: {},
