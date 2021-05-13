2c860f615a2615f8c0c4fcc77f99b60a4cde5ef9
yaser <yaser@pasaportestudio.com>
2021-05-13 10:19:15 -0500

    new new grid

--- src/components/Grid/index.js
+++ src/components/Grid/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import cn from 'classnames'
 function Grid() {
 	return (
 		<div className="wrapper-grid">
@@ -7,10 +7,14 @@ function Grid() {
             <div className="line-top"></div>
 			<div className="h-full w-full relative flex flex-wrap">
 
-                {Array.from(Array(29).keys()).map(e=><div key={e} className="box-y md:md-box-y lg:lg-box-y xl:xl-box-y"></div>)}
+                {Array.from(Array(29).keys()).map(e=><div key={e} className="box-y md:md-box-y lg:lg-box-y xl:xl-box-y">
+                    <div className={cn('absolute h-full line-child-y', {'right-0': e === 28})}></div>
+                </div>)}
 
                 <div className="absolute w-full h-full flex flex-col">
-                {Array.from(Array(40).keys()).map(e=><div key={e} className="pbox-x md:md-pbox-x lg:lg-pbox-x xl:xl-pbox-x"></div>)}
+                {Array.from(Array(40).keys()).map(e=><div key={e} className="box-x md:md-box-x lg:lg-box-x xl:xl-box-x">
+                    <div className="absolute w-full line-child-x"></div>
+                </div>)}
                 </div>
 			</div>
 		</div>
