0c40ade095e11b4a990b0bbab7d560a95740818b
yaser <yaser@pasaportestudio.com>
2021-06-22 15:12:16 -0500

    home and fix styles

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,8 @@ import Head from 'next/head'
 import Image from 'next/image'
 import Grid from '../src/components/Grid'
 import Navbar from '../src/components/Navbar'
+import { Splide, SplideSlide } from '@splidejs/react-splide';
+import '@splidejs/splide/dist/css/themes/splide-default.min.css';
 
 export default function Home() {
   return (
@@ -16,100 +18,203 @@ export default function Home() {
         <div className="relative h-full w-full md:w-[85%] lg:w-[87%] xl:w-[90%]">
             <Grid /> 
             <div className="flex flex-wrap">
-                <div className="grid-img grid-ml-1 grid-mt-1
-                    md:md-grid-img md:md-grid-ml-1 md:md-grid-mt-1
-                    lg:lg-grid-img lg:lg-grid-ml-1 lg:lg-grid-mt-1
-                    xl:xl-grid-img xl:xl-grid-ml-1 xl:xl-grid-mt-1">
-                    <div className="w-full h-0 overflow-hidden relative ar4x6">
-                        <a href="#">
-                            <img src="https://app.yandre.me/uploads/bro/img_grid_1.png" className="w-full h-full absolute top-0 left-0 object-cover" alt=""/>
-                        </a>
+
+            <div className="w-full">
+                    <div className="w-full relative pb-[162.5%] md:pb-[100%] lg:pb-[72.7272%] xl:pb-[55.14%]">
+                        <h1 className="absolute text-5xl sm:text-6xl md:text-[80px] md:leading-none lg:text-7xl xl:text-8xl font-medium tracking-tight
+                        mt-[12.5%] flex justify-center mx-auto inset-x-0
+                        md:w-[75%] md:mt-[15.2%]
+                        lg:block lg:ml-[9.08%] lg:mt-[19.4%]
+                        xl:ml-[6.896%] xl:mt-[11.344%]
+                        "><span>Design <span className="font-sec block">is thinking</span> made <span className="font-sec">visual.</span></span></h1>
+
+                        <p className="absolute text-base xl:text-lg font-medium tracking-tight leading-normal
+                        mt-[63.9%] flex justify-center mx-auto inset-x-0 px-[14%]
+                        md:w-[75%] md:mt-[56.6%] md:px-[3%]
+                        lg:block lg:w-[31.78%] lg:ml-[6.896%] lg:mt-[25%] lg:p-0 lg:right-[10%] lg:left-auto
+                        xl:w-[27.548%] xl:ml-[6.896%] xl:mt-[15.6%] xl:right-[11.2%]
+                        ">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro, delectus. Nesciunt repellat iusto tenetur. Ex consequuntur sequi, dignissimos odit, alias fuga saepe in magni ipsa optio commodi libero aut sapiente.</p>
+
+                        <button className="absolute text-base xl:text-lg font-medium z-20
+                        w-[18%] pb-[18%] mt-[128.4%] flex justify-center items-center mx-auto inset-x-0 
+                        md:w-[6.66%] md:pb-[6.66%] md:mt-[79.92%] 
+                        lg:w-[4.54%] lg:pb-[4.54%] lg:mt-[54.48%] 
+                        xl:w-[3.44%] xl:pb-[3.44%] xl:mt-[44.824%] 
+                        ">
+                            <img className="absolute w-[90%] h-[90%] top-[5%]" src='/img/down-fill.svg' alt='next' />
+                        </button>
+
                     </div>
                 </div>
-
-                <div className="grid-img grid-ml-1 grid-mt-1
-                    md:md-grid-img md:md-grid-ml-1 md:md-grid-mt-2
-                    lg:lg-grid-img lg:lg-grid-ml-1 lg:lg-grid-mt-2
-                    xl:xl-grid-img xl:xl-grid-ml-1 xl:xl-grid-mt-2 
-                ">
-                    <div className="w-full h-0 overflow-hidden relative ar3x3">
-                        <a href="#">
-                            <img src="https://app.yandre.me/uploads/bro/img_grid_2.png" className="w-full h-full absolute top-0 left-0 object-cover" alt=""/>
-                        </a>
-                    </div>
+                {/* ADS */}
+                <div className="w-full">
+                <Splide options={{
+					type: 'loop',
+					// speed: 700,
+					// autoplay: true,
+					// interval: 7000,
+					// pauseOnHover: false,
+					// pauseOnFocus: false,
+					arrows: false,
+					// rewind: true,
+					classes: {
+						pagination: 'splide__pagination hs', // container
+						page: 'splide__pagination__page splide_p-page', // each button
+					},
+				}}>
+  <SplideSlide>
+  <article className="w-full">
+			<div className="w-full h-0 overflow-hidden relative pb-[50%] md:pb-[53.28%] lg:pb-[54.42%] xl:pb-[51.69%]">
+				<img
+					src='/img/COVER.jpg'
+					alt=''
+					className="absolute top-0 w-full h-full"
+					width={1920}
+					height={925}
+				/>
+			</div>
+		</article>
+  </SplideSlide>
+  <SplideSlide>
+  <article className="w-full">
+			<div className="w-full h-0 overflow-hidden relative pb-[50%] md:pb-[53.28%] lg:pb-[54.42%] xl:pb-[51.7272%]">
+				<img
+					src='/img/COVER.jpg'
+					alt=''
+					className="absolute top-0 w-full h-full"
+					width={1920}
+					height={925}
+				/>
+			</div>
+		</article>
+  </SplideSlide>
+</Splide>
                 </div>
+                
+                {/* imgs */}
 
-                <div className="grid-img grid-ml-1 grid-mt-1
-                    md:md-grid-img md:md-grid-ml-1 md:md-grid-mt-1
-                    lg:lg-grid-img lg:lg-grid-ml-1 lg:lg-grid-mt-3
-                    xl:xl-grid-img xl:xl-grid-ml-1 xl:xl-grid-mt-3
+
+                <div className="w-[75%] ml-[12.5%] mt-[12.5%]
+                    md:w-[59.94%] md:ml-[13.32%] md:mt-[6.66%]
+                    lg:w-[40.86%] lg:ml-[4.54%] lg:mr-[54.48%] lg:mt-[4.54%]
+                    xl:w-[27.587%] xl:ml-[10.344%] xl:mr-0 xl:mt-[3.448%]
                     ">
-                    <div className="w-full h-0 overflow-hidden relative ar4x6">
+                    <div className="w-full h-0 relative ar3x3">
                         <a href="#">
-                            <img src="https://app.yandre.me/uploads/bro/img_grid_3.png" className="w-full h-full absolute top-0 left-0 object-cover" alt=""/>
+                            <img src="/img/home_1.jpg" className="w-full h-full absolute top-0 left-0 object-cover" alt=""/>
                         </a>
+                        <div className="absolute bg-black bottom-[-33.33%] w-[100%] right-0 pb-[33.33%] md:w-[22.22%] md:bottom-0 md:right-[-22.22%] md:pb-[22.22%] xl:w-[25%] xl:right-[-25%] xl:pb-[25%]">
+                            <div className="absolute h-full w-full text-left flex justify-center items-center text-white flex-col">
+                                <span className="w-[90%] text-base md:text-sm font-sec">Proyecto casa&nbsp;A</span>
+                                <span className="w-[90%] text-base md:text-xs font-main">Miraflores 2019</span>
+                            </div>
+                        </div>
                     </div>
                 </div>
 
-                <div className="grid-img grid-ml-1 grid-mt-1
-                    md:md-grid-img md:md-grid-ml-1 md:md-grid-mt-n1
-                    lg:lg-grid-img lg:lg-grid-ml-1 lg:lg-grid-mt-n1
-                    xl:xl-grid-img xl:xl-grid-ml-1 xl:xl-grid-mt-2
+                <div className="w-[75%] ml-[12.5%] mt-[37.5%]
+                    md:w-[59.94%] md:ml-[13.32%] md:mt-[19.98%]
+                    lg:w-[40.86%] lg:ml-[45.4%] lg:mt-[9.08%]
+                    xl:w-[27.587%] xl:ml-[17.24%] xl:mt-[13.792%] 
+                ">
+                    <div className="w-full h-0 relative ar3x3">
+                        <a href="#">
+                            <img src="/img/home_2.jpg" className="w-full h-full absolute top-0 left-0 object-cover" alt=""/>
+                        </a>
+                        <div className="absolute bg-black bottom-[-33.33%] w-[100%] right-0 pb-[33.33%] md:w-[22.22%] md:bottom-0 md:right-[-22.22%] md:pb-[22.22%] xl:w-[25%] xl:right-[-25%] xl:pb-[25%]">
+                            <div className="absolute h-full w-full text-left flex justify-center items-center text-white flex-col">
+                                <span className="w-[90%] text-base md:text-sm font-sec">Proyecto casa&nbsp;B</span>
+                                <span className="w-[90%] text-base md:text-xs font-main">Miraflores 2019</span>
+                            </div>
+                        </div>
+                        <div className="z-[-1] absolute md:top-[-24.2%] md:w-[52.28%] md:right-[-26.22%] md:pb-[60.22%] xl:top-[-25%] xl:w-[52.5%] xl:right-[-27.8%] xl:pb-[62.5%]">
+                            <img src="/img/img_v.png" className="w-full h-full absolute top-0 left-0 object-cover" alt=""/>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="hidden lg:block
+                    lg:w-[22.7%] lg:ml-[4.54%] lg:mr-[54.48%] lg:mt-[-27.24%]
+                    xl:w-[13.792%] xl:ml-[17.24%] xl:mr-0 xl:mt-[-6.896%]
                     ">
-                    <div className="w-full h-0 overflow-hidden relative ar4x6">
-                        <a href="#">
-                            <img src="https://app.yandre.me/uploads/bro/img_grid_4.png" className="w-full h-full absolute top-0 left-0 object-cover" alt=""/>
-                        </a>
+                    <div className="w-full h-0 relative pb-[120%] xl:pb-[125%]">
+                        <img src="/img/img_a.png" className="w-full h-full absolute top-0 left-0 object-cover" alt=""/>
                     </div>
                 </div>
 
-                <div className="grid-img grid-ml-1 grid-mt-1
-                md:md-grid-img md:md-grid-ml-1 md:md-grid-mt-1
-                lg:lg-grid-img lg:lg-grid-ml-1 lg:lg-grid-mt-n3
-                xl:xl-grid-img xl:xl-grid-ml-1 xl:xl-grid-mt-n1
-                ">
-                    <div className="w-full h-0 overflow-hidden relative ar3x3 lg:ar4x6 xl:ar3x3">
+                <div className="w-[75%] ml-[12.5%] mb-[37.5%] mt-[37.5%]
+                    md:w-[59.94%] md:ml-[13.32%] md:mb-0 md:mt-[13.32%]
+                    lg:w-[40.86%] lg:ml-[4.54%] lg:mr-[9.08%] lg:mt-[9.08%]
+                    xl:w-[27.587%] xl:ml-[27.584%] xl:mr-0 xl:mt-[6.896%]
+                    ">
+                    <div className="w-full h-0 relative ar3x3">
                         <a href="#">
-                            <img src="https://app.yandre.me/uploads/bro/img_grid_5.png" className="w-full h-full absolute top-0 left-0 object-cover" alt=""/>
+                            <img src="/img/home_3.jpg" className="w-full h-full absolute top-0 left-0 object-cover" alt=""/>
                         </a>
+                        <div className="absolute bg-black bottom-[-33.33%] w-[100%] right-0 pb-[33.33%] md:w-[22.22%] md:bottom-0 md:right-[-22.22%] md:pb-[22.22%] xl:w-[25%] xl:right-[-25%] xl:pb-[25%]">
+                            <div className="absolute h-full w-full text-left flex justify-center items-center text-white flex-col">
+                                <span className="w-[90%] text-base md:text-sm font-sec">Proyecto casa&nbsp;C</span>
+                                <span className="w-[90%] text-base md:text-xs font-main">Miraflores 2019</span>
+                            </div>
+                        </div>
                     </div>
                 </div>
 
-                <div className="grid-img grid-ml-1 grid-mt-1
-                md:md-grid-img md:md-grid-ml-1 md:md-grid-mt-n1
-                lg:lg-grid-img lg:lg-grid-ml-1 lg:lg-grid-mt-1
-                xl:xl-grid-img xl:xl-grid-ml-1 xl:xl-grid-mt-n3
-                ">
-                    <div className="w-full h-0 overflow-hidden relative ar4x6">
+                <div className="hidden
+                    xl:block xl:w-[27.587%] xl:ml-[13.792%] xl:mt-[-17.24%] 
+                    ">
+                    <div className="w-full h-0 relative ar3x3">
                         <a href="#">
-                            <img src="https://app.yandre.me/uploads/bro/img_grid_6.png" className="w-full h-full absolute top-0 left-0 object-cover" alt=""/>
+                            <img src="/img/home_4.jpg" className="w-full h-full absolute top-0 left-0 object-cover" alt=""/>
                         </a>
+                        <div className="absolute bg-black bottom-[-33.33%] w-[100%] right-0 pb-[33.33%] md:w-[22.22%] md:bottom-0 md:right-[-22.22%] md:pb-[22.22%] xl:w-[25%] xl:right-[-25%] xl:pb-[25%]">
+                            <div className="absolute h-full w-full text-left flex justify-center items-center text-white flex-col">
+                                <span className="w-[90%] text-base md:text-sm font-sec">Proyecto casa&nbsp;D</span>
+                                <span className="w-[90%] text-base md:text-xs font-main">Miraflores 2019</span>
+                            </div>
+                        </div>
                     </div>
                 </div>
 
-                <div className="grid-img grid-ml-1 grid-mt-1 grid-mb-0
-                md:md-grid-img md:md-grid-ml-1 md:grid-mt-0
-                lg:lg-grid-img lg:lg-grid-ml-1 lg:lg-grid-mt-n1
-                xl:xl-grid-img xl:xl-grid-ml-1 xl:xl-grid-mt-1 xl:xl-grid-mb-1
-                ">
-                    <div className="w-full h-0 overflow-hidden relative ar3x3">
-                        <a href="#">
-                            <img src="https://app.yandre.me/uploads/bro/img_grid_7.png" className="w-full h-full absolute top-0 left-0 object-cover" alt="" />
-                        </a>
+                <div className="hidden xl:block
+                    xl:w-[6.896%] xl:ml-[31.032%] xl:mr-0 xl:mt-[3.448%]
+                    ">
+                    <div className="w-full h-0 relative pb-[150%] xl:pb-[150%]">
+                        <img src="/img/img_v2.png" className="w-full h-full absolute top-0 left-0 object-cover" alt=""/>
                     </div>
                 </div>
 
-                <div className="grid-img grid-ml-1 grid-mt-1 grid-mb-1
-                md:md-grid-img md:md-grid-ml-1 md:md-grid-mt-1 md:md-grid-mb-1
-                lg:lg-grid-img lg:lg-grid-ml-1 lg:lg-grid-mt-n3 lg:lg-grid-mb-1
-                xl:xl-grid-img xl:xl-grid-ml-1 xl:grid-mt-0 xl:xl-grid-mb-1
-                ">
-                    <div className="w-full h-0 overflow-hidden relative ar3x3 lg:ar4x6 xl:ar3x3">
-                        <a href="#">
-                            <img src="https://app.yandre.me/uploads/bro/img_grid_8.png" className="w-full h-full absolute top-0 left-0 object-cover" alt=""/>
-                        </a>
-                    </div>
+                <div className="w-[50%] mx-auto mt-0
+                    md:w-[33.3%] md:mx-auto md:mt-[6.66%]
+                    lg:w-[22.7%] lg:ml-[9.08%] lg:mr-[4.54%] lg:mt-[45.4%]
+                    xl:w-[10.344%] xl:ml-[3.448%] xl:mr-0 xl:mt-[10.344%]
+                    ">
+                    <button className="w-full bg-[#FDD704] relative h-0 flex pb-[25%] md:pb-[20%] xl:pb-[33.33%]">
+                        <div className="absolute h-full w-[75%] md:w-[80%] xl:w-[66.66%] text-center text-sm md:text-base xl:text-xs font-sec inline-flex justify-center items-center">
+                            Ver más proyectos
+                        </div>
+                        <div className="absolute h-full w-[25%] md:w-[20%] xl:w-[33.33%] text-center text-xl font-third inline-flex justify-center items-center right-0">
+                            <span className="transform scale-[1.5]">+</span>
+                        </div>
+                    </button>
                 </div>
+
+
+                {/* LAST */}
+                <article className="w-full">
+                    <div className="w-full h-0 overflow-hidden relative pb-[50%] 
+                    md:pb-[39.96%] 
+                    lg:pb-[36.32%] 
+                    xl:pb-[31.032%]">
+                        <img
+                            src='/img/FOOTER.jpg'
+                            alt=''
+                            className="absolute top-0 w-full h-full"
+                            width={1920}
+                            height={925}
+                        />
+                    </div>
+                </article>
             </div>
         </div>
       </main>
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -40,11 +40,12 @@ export default function Named() {
             <div className={
                 cn('w-full order-1 md:order-first flex justify-between', {'mt-2': open && isMobile})
             }>
-                <h1 className="font-semibold text-3xl
-                    md:text-2xl md:mt-1 xl:mt-2 md:mb-10
-                    xl:text-[1.8vw]">Visualiza<span className="text-yellow-400 text-4xl">.</span></h1>
+                <h2 className="font-semibold text-xl
+                    md:mt-1 xl:mt-2 md:mb-10 w-full">
+                    <img className="w-[50%] md:w-[95%]" src='/img/logo_visualiza.svg' alt='Visualiza' />
+                    </h2>
                 <span
-					className={cn('flex items-center z-10', {hidden: !isMobile})}
+					className={cn('flex items-center z-10 cursor-pointer', {hidden: !isMobile})}
 					onClick={() => {
                         setOpen(!open)
                         open ? controls.start('hidden') : controls.start('visible')
@@ -55,7 +56,7 @@ export default function Named() {
 				</span>
             </div>
             <motion.nav variants={variants} animate={controls} initial="hidden" className={
-                cn('-mt-4 -ml-4 -mr-5 w-[110%] bg-black md:w-full md:bg-transparent md:m-0',
+                cn('-mt-4 -ml-3 -mr-3 w-[calc(100%+6rem)] bg-black md:w-full md:bg-transparent md:m-0',
                     'md:clear-trs',
                 )
             }>
@@ -65,22 +66,22 @@ export default function Named() {
                 )}>
                     <li className="py-3 md:py-1 font-semibold leading-relaxed text-base xl:text-[1vw]">
                         <Link href="#">
-                            <a className={cn({'hover:text-white': isMobile}, {'text-white': router.pathname === '/' && isMobile})}>Nosotros</a>
+                            <a className={cn('font-sec',{'hover:text-white': isMobile}, {'text-white': router.pathname === '/' && isMobile})}>Nosotros</a>
                         </Link>
                     </li>
                     <li className="py-3 md:py-1 font-semibold leading-relaxed text-base xl:text-[1vw]">
                         <Link href="#">
-                            <a className={cn({'hover:text-white': isMobile})}>Proyectos</a>
+                            <a className={cn('font-sec',{'hover:text-white': isMobile})}>Proyectos</a>
                         </Link>
                     </li>
                     <li className="py-3 md:py-1 font-semibold leading-relaxed text-base xl:text-[1vw]">
                         <Link href="#">
-                            <a className={cn({'hover:text-white': isMobile})}>Servicios</a>
+                            <a className={cn('font-sec',{'hover:text-white': isMobile})}>Servicios</a>
                         </Link>
                     </li>
                     <li className="py-3 md:py-1 font-semibold leading-relaxed text-base xl:text-[1vw]">
                         <Link href="#">
-                            <a className={cn({'hover:text-white': isMobile})}>Contacto</a>
+                            <a className={cn('font-sec',{'hover:text-white': isMobile})}>Contacto</a>
                         </Link>
                     </li>
                 </ul>
